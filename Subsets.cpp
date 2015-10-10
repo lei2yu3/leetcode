9aba9c3b9700f30441cc19f1443d9eeb7771d596
malloc <leiyu.cpp@gmail.com>
2015-10-10 11:44:18 +0800

    Update Subsets.cpp

--- Subsets.cpp
+++ Subsets.cpp
@@ -1,3 +1,34 @@
+/*
+
+Given a set of distinct integers, S, return all possible subsets.
+
+Note:
+Elements in a subset must be in non-descending order.
+The solution set must not contain duplicate subsets.
+For example,
+If S = [1,2,3], a solution is:
+
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+
+求合集的子集（包括空集和本省），二维向量
+
+合集元素个数为N，则子集个数为1 << N - 1 = 2^N
+N=3时, 子集个数2^N = 8个，子集 = {{}, {1}, {2}, {3}, {1,2}, {1,3}, {2,3}, {1,2,3}}
+其二进制表示 {{0,0,0}, {1,0,0}, {0,1,0}, {0,0,1}, {1,1,0}, {1,0,1}, {0,1,1}, {1,1,1}}，1代表集合中有该元素，0代表没有
+遍历所有子集的二进制表示，查看哪一位是1
+
+*/
+
+
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int> &S)
