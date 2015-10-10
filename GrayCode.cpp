8faf5602d80343ff3a90352403e6bd8c3b72197a
malloc <leiyu.cpp@gmail.com>
2015-10-10 13:50:09 +0800

    Update GrayCode.cpp

--- GrayCode.cpp
+++ GrayCode.cpp
@@ -1,3 +1,49 @@
+/*
+
+The gray code is a binary numeral system where two successive values differ in only one bit.
+
+Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
+
+For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+
+00 - 0
+01 - 1
+11 - 3
+10 - 2
+Note:
+For a given n, a gray code sequence is not uniquely defined.
+
+For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
+
+For now, the judge is able to judge based on one instance of gray code sequence. Sorry about that.
+
+Gray Code
+http://en.wikipedia.org/wiki/Gray_code
+
+
+    // The purpose of this function is to convert an unsigned
+    // binary number to reflected binary Gray code.
+    // The operator >> is shift right. The operator ^ is exclusive or.
+unsigned int binaryToGray(unsigned int num)
+{
+        return (num >> 1) ^ num;
+}
+
+    // The purpose of this function is to convert a reflected binary
+    // Gray code number to a binary number.
+unsigned int grayToBinary(unsigned int num)
+{
+    unsigned int mask;
+    for (mask = num >> 1; mask != 0; mask = mask >> 1)
+    {
+        num = num ^ mask;
+    }
+    return num;
+}
+
+*/
+
+
 class Solution {
 public:
     vector<int> grayCode(int n) {
