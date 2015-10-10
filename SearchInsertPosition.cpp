613a8139f00c8f3c7fcdb3de9a4e4d0bf96090d7
malloc <leiyu.cpp@gmail.com>
2015-10-10 11:46:05 +0800

    Update SearchInsertPosition.cpp

--- SearchInsertPosition.cpp
+++ SearchInsertPosition.cpp
@@ -1,3 +1,22 @@
+/*
+
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+
+有序数列的查找。
+若找到则返回位置，若未找到则返回可以被插入的位置。
+binary search may be better
+
+*/
+
+
 class Solution {
 public:
     int searchInsert(int A[], int n, int target) {
