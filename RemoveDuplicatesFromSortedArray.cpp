140e14bb1616d2904550ab57e7eda3e9e2345ff9
malloc <leiyu.cpp@gmail.com>
2015-10-10 13:42:46 +0800

    Update RemoveDuplicatesFromSortedArray.cpp

--- RemoveDuplicatesFromSortedArray.cpp
+++ RemoveDuplicatesFromSortedArray.cpp
@@ -1,3 +1,19 @@
+/*
+
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array A = [1,1,2],
+
+Your function should return length = 2, and A is now [1,2].
+
+除去排序数组中重复的内容，使数组中的内容只出现一次，输出处理之后数组的长度
+
+*/
+
+
 class Solution {
 public:
     int removeDuplicates(int A[], int n) {
