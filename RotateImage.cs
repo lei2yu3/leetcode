c9dd83fb2cfea72f750f87c0126609b2bdda75ab
malloc <leiyu.cpp@gmail.com>
2015-10-10 11:51:52 +0800

    Update RotateImage.cs

--- RotateImage.cs
+++ RotateImage.cs
@@ -1,3 +1,22 @@
+/*
+
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Follow up:
+Could you do this in-place?
+
+二维数组顺时针旋转90度，即(i, j)与(j, m-i)对换
+两种办法：
+1. 先上下翻转，即(i, j)与(m-i, j)对换；再沿左上到右下的对角线翻转，即(i, j)与(j, i)对换。
+2. 先沿右上到左下的对角线翻转，即(i, j)与(n-j, m-i)对换；再上下翻转。
+
+其他解法：
+http://www.cnblogs.com/chrischennx/p/4009376.html
+
+*/
+
 public class Solution {
     public void Rotate(int[,] matrix) {
         
