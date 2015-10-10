4f379b7ed5ced3ed7b88c6fe3e7012776ee392c1
malloc <leiyu.cpp@gmail.com>
2015-10-10 13:44:24 +0800

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,3 +1,23 @@
+/*
+
+Determine whether an integer is a palindrome. Do this without extra space.
+
+Some hints:
+Could negative integers be palindromes? (ie, -1)
+
+If you are thinking of converting the integer to string, note the restriction of using extra space.
+
+You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+
+There is a more generic way of solving this problem.
+
+将Integer转换为String
+分别从头向后和从尾向前比较每一位的字符是否相同
+s.charAt(i)
+
+*/
+
+
 public class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0)
