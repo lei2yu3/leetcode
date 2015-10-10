3436076e8bb89d451b7b16f646a97383efa9aaf1
malloc <leiyu.cpp@gmail.com>
2015-10-10 13:41:39 +0800

    Update ReverseInteger.java

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,3 +1,31 @@
+/*
+
+Reverse digits of an integer.
+
+Example1: x = 123, return 321
+Example2: x = -123, return -321
+
+
+Have you thought about this?
+Here are some good questions to ask before coding. Bonus points for you if you have already thought through this!
+
+If the integer's last digit is 0, what should the output be? ie, cases such as 10, 100.
+
+Did you notice that the reversed integer might overflow? Assume the input is a 32-bit integer, then the reverse of 1000000003 overflows. How should you handle such cases?
+
+For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+
+整型数字逆序
+
+将Integer转换为String，再转换为CharArray，然后再逆向连接为String，最后转换回Integer。
+String.valueOf(x);
+s.toCharArray();
+Integer.parseInt(ss)
+
+*/
+
+
 public class Solution {
     public int reverse(int x) {
                 if (x >= -9 && x <= 9)
