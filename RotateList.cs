20783e7beced8332149b921fcbaedfe70c9cbbd9
malloc <leiyu.cpp@gmail.com>
2015-10-10 11:48:29 +0800

    Update RotateList.cs

--- RotateList.cs
+++ RotateList.cs
@@ -1,3 +1,18 @@
+/*
+
+Given a list, rotate the list to the right by k places, where k is non-negative.
+
+For example:
+Given 1->2->3->4->5->NULL and k = 2,
+return 4->5->1->2->3->NULL.
+
+循环右移链表
+获取链表长度length，将链表首尾相连形成环
+根据k值，重新设置head
+
+*/
+
+
 public class Solution {
     public ListNode RotateRight(ListNode head, int k)
     {
