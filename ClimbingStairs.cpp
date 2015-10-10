ca74c03cf6c434df58df7129c703a7b33f0da3d6
malloc <leiyu.cpp@gmail.com>
2015-10-10 13:53:23 +0800

    Update ClimbingStairs.cpp

--- ClimbingStairs.cpp
+++ ClimbingStairs.cpp
@@ -1,3 +1,17 @@
+/*
+
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+经推导，结果为斐波那契数列
+
+分析过程：
+http://blog.csdn.net/wangshihui512/article/details/9672471
+
+*/
+
+
 class Solution {
 public:
     int climbStairs(int n) {
