d23f0c77b9caa87e7b580af404f3152338f6e6b3
malloc <leiyu.cpp@gmail.com>
2015-10-10 13:52:32 +0800

    Update ExcelSheetColumnNumber.cpp

--- ExcelSheetColumnNumber.cpp
+++ ExcelSheetColumnNumber.cpp
@@ -1,3 +1,22 @@
+/*
+
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+
+26进制转换为10进制
+
+*/
+
+
 class Solution {
 public:
     int titleToNumber(string s) {
