afd92a084b2c98074b0852b424dc8d47dc165991
malloc <leiyu.cpp@gmail.com>
2015-10-10 13:45:28 +0800

    Update NumberOf1Bits.cs

--- NumberOf1Bits.cs
+++ NumberOf1Bits.cs
@@ -1,3 +1,16 @@
+/*
+
+Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+
+For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+
+判断二进制数字中有几个1
+
+不断右移，和1进行"与运算"
+
+*/
+
+
 public class Solution {
     public int HammingWeight(uint n) 
     {
