64c48cb577fc3d5465d15a0c1ab8066d169fc684
malloc <leiyu.cpp@gmail.com>
2015-10-10 13:47:33 +0800

    Update IntegerToRoman.java

--- IntegerToRoman.java
+++ IntegerToRoman.java
@@ -1,8 +1,24 @@
 /*
+
 Given an integer, convert it to a roman numeral.
 
 Input is guaranteed to be within the range from 1 to 3999.
+
+字符串转换
+
+wiki of roman numeral:
+http://zh.wikipedia.org/zh/%E7%BD%97%E9%A9%AC%E6%95%B0%E5%AD%97
+
+阿拉伯数字转化为罗马数字
+分别对个十百千位进行转换。
+
+亦可分为：
+int A[] = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
+String R[] = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
+若num大于某个值A，则取其对应罗马数字R然后num减A，直到num等于1。
+
 */
+
 
 public class Solution {
     public String intToRoman(int num) {
