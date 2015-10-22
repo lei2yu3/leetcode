e4c1c2390c8760223220153200d81aa1c18afd9f
malloc <leiyu.cpp@gmail.com>
2015-10-22 17:28:52 +0800

    Update BestTimeBuyAndSellStockII.cs

--- BestTimeBuyAndSellStockII.cs
+++ BestTimeBuyAndSellStockII.cs
@@ -8,3 +8,23 @@ However, you may not engage in multiple transactions at the same time
 (ie, you must sell the stock before you buy again).
 
 */
+
+
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int profit = 0;
+        if (prices.Length < 2) return profit;
+        
+        int min = prices[0];
+        
+        for (int i = 0; i < prices.Length - 1; i++)
+        {
+        if (prices[i + 1] - prices[i] > 0)
+            profit += prices[i + 1] - prices[i];
+        else if (prices[i + 1] - prices[i] < 0) 
+            continue;
+        }
+        
+        return profit;    
+    }
+}
