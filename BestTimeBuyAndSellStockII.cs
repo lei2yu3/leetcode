14ec6e6130729981a5b485075e738874527551d8
malloc <leiyu.cpp@gmail.com>
2015-10-23 10:54:13 +0800

    Update BestTimeBuyAndSellStockII.cs

--- BestTimeBuyAndSellStockII.cs
+++ BestTimeBuyAndSellStockII.cs
@@ -7,22 +7,27 @@ Design an algorithm to find the maximum profit. You may complete as many transac
 However, you may not engage in multiple transactions at the same time 
 (ie, you must sell the stock before you buy again).
 
+数组第i个元素表示货物第i天的价格，进行多次买入和卖出，使得收益最大
+
+不买入不能卖出，当天卖出不能当天买入，买入则必须卖出（买入卖出须成对出现且不能在同一天进行）
+
+寻找数列中的所有递增子数列，各个字数列最右减最左得收益，再求收益和
+
+如果当前元素比下一个元素小，则总收益增加二者的差
+
 */
 
 
 public class Solution {
     public int MaxProfit(int[] prices) {
+        
         int profit = 0;
         if (prices.Length < 2) return profit;
         
-        int min = prices[0];
-        
         for (int i = 0; i < prices.Length - 1; i++)
         {
-        if (prices[i + 1] - prices[i] > 0)
-            profit += prices[i + 1] - prices[i];
-        else if (prices[i + 1] - prices[i] < 0) 
-            continue;
+            if (prices[i + 1] - prices[i] > 0)
+                profit += prices[i + 1] - prices[i];
         }
         
         return profit;    
