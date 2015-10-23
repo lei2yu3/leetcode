5d37582c597ac4c2d78bd98b8e6382f55ce3ef2f
malloc <leiyu.cpp@gmail.com>
2015-10-23 12:00:08 +0800

    Update BestTimeBuyAndSellStockIII.cs

--- BestTimeBuyAndSellStockIII.cs
+++ BestTimeBuyAndSellStockIII.cs
@@ -7,4 +7,6 @@ Design an algorithm to find the maximum profit. You may complete at most two tra
 Note:
 You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
 
+数组第i个元素表示货物第i天的价格，进行至最多两次买入和卖出，使得收益最大
+
 */
