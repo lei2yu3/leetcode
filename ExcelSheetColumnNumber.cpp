aaaad8d04d2bbdfa334cda51c23b423e2b476e5d
malloc <leiyu.cpp@gmail.com>
2015-11-03 11:11:36 +0800

    Update ExcelSheetColumnNumber.cpp

--- ExcelSheetColumnNumber.cpp
+++ ExcelSheetColumnNumber.cpp
@@ -17,19 +17,22 @@ AB -> 28
 */
 
 
-class Solution {
+class Solution 
+{
 public:
-    int titleToNumber(string s) {
+    int titleToNumber(string s) 
+    {
         if(s.size() == 0)
             return 0;
     
         int size = s.size();
         int num = 0;
-        for(int i = 0; i < size; i++){
+        for(int i = 0; i < size; i++)
+        {
             num += (s[i] - 64) * pow(26, size-1-i);
             // num = num * 26 + s[i] - 64; // without inlucde <cmath> and using pow()
         }
         
         return num;
-        }
+    }
 };
