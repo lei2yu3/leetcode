8379dbbb13ba4e025df367626729f69668d2c50e
malloc <leiyu.cpp@gmail.com>
2015-11-16 15:06:34 +0800

    Update SimplifyPath.cs

--- SimplifyPath.cs
+++ SimplifyPath.cs
@@ -12,4 +12,43 @@ In this case, you should return "/".
 Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
 In this case, you should ignore redundant slashes and return "/home/foo".
 
+以"/"为分隔符将字符串拆分，若是目录则入栈，若是".."则出栈，若是"."则无操作。
+
 */
+
+
+public class Solution 
+{
+    public string SimplifyPath(string path) 
+    {
+        Stack<string> stack = new Stack<string>();
+
+        foreach (var s in path.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries))
+        {
+            if (s.Equals(".."))
+            {
+                if (stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+            }
+            else if (s.Equals("."))
+            {
+                continue;
+            }
+            else
+            {
+                stack.Push(s);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        foreach (var s in stack)
+        {
+            sb.Insert(0, "/" + s);
+        }
+
+        return sb.Length == 0 ? "/" : sb.ToString();
+    }
+}
